67893ab0c56fe97b7eedef5dede76c443e8d841f
Soichi Ikebe <souichi1997@gmail.com>
2024-06-27 03:52:56 +0900

    Implement View and ViewModel

---
+++ MauiPlayground/Views/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace MauiPlayground.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            Console.WriteLine("OnBackButtonPressed()");
+            return false;
+        }
+    }
+
+}
