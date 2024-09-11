8a4161108198cffbf1edee7f0b6a848e5de66be2
Soichi Ikebe <souichi1997@gmail.com>
2024-09-11 10:33:42 +0900

    Define activity name and add export setting

--- MauiPlayground/Platforms/Android/MainActivity.cs
+++ MauiPlayground/Platforms/Android/MainActivity.cs
@@ -4,7 +4,13 @@ using Android.OS;
 
 namespace MauiPlayground
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Activity(Name = "com.companyname.mauiplayground.MainActivity",
+        Theme = "@style/Maui.SplashTheme",
+        Exported = true,
+        LaunchMode = LaunchMode.SingleTop,
+        MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode |
+                               ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
     }
